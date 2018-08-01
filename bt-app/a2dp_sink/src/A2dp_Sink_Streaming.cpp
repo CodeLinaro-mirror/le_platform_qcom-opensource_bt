98e8a30ce099292b67ee591acceb277719113440
AnubhavGupta <anubhavg@codeaurora.org>
2018-08-02 00:08:44 +0530

    A2DP_SINK: Enable Audio integration
     - enable AUDIO_HAL_INTEGRATION based on target type
     - change out device to LINE_OUT
    
    Change-Id: Ia73fe10c445df46ee51790dc86c4b245617b2080

--- bt-app/a2dp_sink/src/A2dp_Sink_Streaming.cpp
+++ bt-app/a2dp_sink/src/A2dp_Sink_Streaming.cpp
@@ -1015,7 +1015,8 @@ void A2dp_Sink_Streaming::ConfigureAudioHal() {
             }
             // 2 refers to speaker
             ALOGD(LOGTAG " opening output stream ");
-            qahw_open_output_stream(audio_device, handle, 2, (audio_output_flags_t)flags,
+            qahw_open_output_stream(audio_device, handle, OUT_DEVICE_LINE_OUT,
+                   (audio_output_flags_t)flags,
                    &config, &out_stream, "bt_a2dp_sink");
         }
         if (out_stream != NULL) {
