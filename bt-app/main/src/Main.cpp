0d9c9b30d719d27d4bacfaec34ca96eb0bb86699
haoll <haoll@codeaurora.org>
2018-12-05 13:57:25 +0800

    fix ble failed to start after disable bt.
    disable rsp profile before disable bt.
    
    CRs-Fixed: 2360732
    
    Change-Id: I3eecd4e57b8e619c5a351bfca59e374908f4a89e
    Signed-off-by: haoll <haoll@codeaurora.org>

--- bt-app/main/src/Main.cpp
+++ bt-app/main/src/Main.cpp
@@ -1576,6 +1576,24 @@ static void SendDisableCmdToGap() {
     if ((g_bt_app->status.disable_cmd != COMMAND_INPROGRESS) &&
         (g_bt_app->status.enable_cmd != COMMAND_INPROGRESS) &&
         (g_bt_app->bt_state == BT_STATE_ON)) {
+        if (gattstest) {
+            fprintf(stdout, " DisableGATTSTEST \n");
+            gattstest->DisableGATTSTEST();
+        } else {
+            ALOGV (LOGTAG " gattstest interface is null");
+        }
+        if (rsp) {
+            rsp->DisableRSP();
+            fprintf(stdout, " DisableRSP \n");
+        } else {
+            ALOGV (LOGTAG " rsp interface is null");
+        }
+        if (gattctest) {
+             fprintf(stdout, " DisableGATTCTEST \n");
+             gattctest->DisableGATTCTEST();
+        } else {
+             ALOGV (LOGTAG " gattctest interface is null");
+        }
 
         g_bt_app->status.disable_cmd = COMMAND_INPROGRESS;
 
@@ -1608,57 +1626,6 @@ static void HandleGapCommand(int cmd_id, char user_cmd[][COMMAND_ARG_SIZE]) {
         case BT_DISABLE:
             SendDisableCmdToGap();
 
-            if ((g_bt_app->status.disable_cmd != COMMAND_INPROGRESS) &&
-                                (g_bt_app->bt_state == BT_STATE_ON)) {
-
-                if (gattstest) {
-                    fprintf(stdout, " DisableGATTSTEST \n");
-                    gattstest->DisableGATTSTEST();
-                } else {
-                    ALOGV (LOGTAG " gattstest interface is null");
-                }
-                if (rsp) {
-                    rsp->DisableRSP();
-                    fprintf(stdout, " DisableRSP \n");
-                } else {
-                    ALOGV (LOGTAG " rsp interface is null");
-                }
-                if (gattctest) {
-                    fprintf(stdout, " DisableGATTCTEST \n");
-                    gattctest->DisableGATTCTEST();
-                } else {
-                    ALOGV (LOGTAG " gattctest interface is null");
-                }
-                g_bt_app->status.disable_cmd = COMMAND_INPROGRESS;
-
-                if (gattstest) {
-                    fprintf(stdout, " DisableGATTSTEST \n");
-                    gattstest->DisableGATTSTEST();
-                } else {
-                    ALOGV (LOGTAG " gattstest interface is null");
-                }
-                if (rsp) {
-                    rsp->DisableRSP();
-                    fprintf(stdout, " DisableRSP \n");
-                } else {
-                    ALOGV (LOGTAG " rsp interface is null");
-                }
-                if (gattctest) {
-                    fprintf(stdout, " DisableGATTCTEST \n");
-                    gattctest->DisableGATTCTEST();
-                } else {
-                    ALOGV (LOGTAG " gattctest interface is null");
-                }
-
-                event = new BtEvent;
-                event->event_id = GAP_API_DISABLE;
-                ALOGV (LOGTAG " Posting disable to GAP thread");
-                PostMessage (THREAD_ID_GAP, event);
-            } else if (g_bt_app->status.disable_cmd == COMMAND_INPROGRESS) {
-                fprintf( stdout, " disable command is already in process\n");
-            } else {
-                fprintf( stdout, "Currently BT is already OFF\n");
-            }
             break;
 
         case START_ENQUIRY:
