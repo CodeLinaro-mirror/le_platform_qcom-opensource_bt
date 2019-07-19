9410722571fbb2f3867987556af661dbbc3db3e2
Harish Bandi <c-hbandi@codeaurora.org>
2019-07-19 11:24:11 +0530

    GATT: Clean the Gatt instances in BT disable
    Added code to delete the instances for GATT server and GATT client
    
    Change-Id: I67a10f72b1233080a62b9c87a3845a2279675b1c

--- bt-app/gattctest/src/GattcTest.cpp
+++ bt-app/gattctest/src/GattcTest.cpp
@@ -1710,13 +1710,14 @@ void GattcTest :: stopScan()
   mScanner->stopScan(mscan_callback);
 }
 
-void GattcTest :: testBatchscan()
+void GattcTest :: testBatchscan(int value)
 {
   ALOGD(LOGTAG "Test Batch scan Mode");
   fprintf(stdout, "Test Batch scan\n");
   ScanSettings *batchscansettings = ScanSettings::Builder()
     .setScanMode(ScanSettings::SCAN_MODE_BALANCED)
     .setReportDelay(BATCH_SCAN_REPORT_DELAY_MILLIS)
+    .setScanResultType(value)
     .build();
   vector < ScanFilter*> filters;
   filters.clear();
