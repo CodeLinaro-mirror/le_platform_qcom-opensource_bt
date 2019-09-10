1cce63928511b3ac97b81696914a1102fcd9ba92
Zijun Hu <zijuhu@codeaurora.org>
2019-09-10 14:49:26 +0800

    bt-app: Fix BT enable failure after enabling WLAN
    Fix BT enable failure after enabling WLAN.
    
    CRs-Fixed: 2521470
    Change-Id: Iaf90b2902fc0aa36d6dd9d115ec2ac7864b1e6e5

--- bt-app/gap/include/Gap.hpp
+++ bt-app/gap/include/Gap.hpp
@@ -49,7 +49,7 @@
 #define MAX_BONDED_DEVICES (20)
 #define PROFILE_STARTUP_TIMEOUT_DELAY     (5000)
 #define PROFILE_STOP_TIMEOUT_DELAY        (5000)
-#define ENABLE_TIMEOUT_DELAY              (12000)
+#define ENABLE_TIMEOUT_DELAY              (40000)
 #define DISABLE_TIMEOUT_DELAY             (8000)
 
 const unsigned char g_audiosink_uuid[16] = {0x00, 0x00, 0x11, 0x0B, 0x00, 0x00,
