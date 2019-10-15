cce6fa0a22924146ff9b04a5be9341318c0fb920
Harish Bandi <hbandi@codeaurora.org>
2019-10-15 17:56:34 +0530

    GATTClient: Check the mservice pointer properly
    We need to call read/write characterstic only if mservice
    is not available.
    
    CRs-Fixed: 2566205
    Change-Id: I5763b99a5a88242b76aa93798d0f3670ac1db0c6

--- bt-app/gatt/framework/src/GattClient.cpp
+++ bt-app/gatt/framework/src/GattClient.cpp
@@ -201,7 +201,7 @@ void GattClient::onCharacteristicRead(string address, int status, int handle, ui
        std::lock_guard<std::mutex> myLock(mDeviceBusyLock);
        mDeviceBusy = true;
       }
-      if(!mService)
+      if(mService != NULL)
         mService->readCharacteristic(mClientIf, address, handle, authReq);
       {
         std::lock_guard<std::mutex> myLock(mDeviceBusyLock);
@@ -260,7 +260,7 @@ void GattClient::onCharacteristicWrite(string address, int status, int handle)
        mDeviceBusy = true;
       }
 
-      if(!mService)
+      if(mService != NULL)
         mService->writeCharacteristic(mClientIf, address, handle,
               characteristic->getWriteType(), authReq,
               characteristic->getValue(), characteristic->getValueLength());
@@ -332,7 +332,7 @@ void GattClient::onDescriptorRead(string address, int status, int handle, uint8_
        mDeviceBusy = true;
       }
 
-      if(!mService) {
+      if(mService != NULL) {
         mService->readDescriptor(mClientIf, address, handle, authReq);
         {
         std::lock_guard<std::mutex> myLock(mDeviceBusyLock);
@@ -390,7 +390,7 @@ void GattClient::onDescriptorWrite(string address, int status, int handle)
        mDeviceBusy = true;
       }
 
-      if(!mService)
+      if(mService != NULL)
         mService->writeDescriptor(mClientIf, address, handle,
               authReq, descriptor->getValue(), descriptor->getValueLength());
       {
