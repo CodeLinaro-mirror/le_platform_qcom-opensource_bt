5948a871e58c5507a0ed06294fd6c826cda4975d
Lakshmi Venkata Sai Kiran Challa <lchalla@codeaurora.org>
2020-08-03 13:45:08 +0530

    BTAPP: GATT: Fix kw issue in Gatt characteristic
    Fix varible mValue accesssed before initialized.
    
    Change-Id: I50dbd7f5b3a65a73ce9beb9444e4f1681eca42f8

--- bt-app/gatt/framework/src/GattCharacteristic.cpp
+++ bt-app/gatt/framework/src/GattCharacteristic.cpp
@@ -243,8 +243,8 @@ bool GattCharacteristic::setValue(int value, int formatType, int offset)
   int len = offset + getTypeLen(formatType);
 
   if (mValue == NULL) mValue = new uint8_t[len];
-
-  if (len > mValueLength) return false;
+  if (len > sizeof(mValue)/sizeof(mValue[0]))
+    return false;
 
   switch (formatType) {
     case FORMAT_SINT8:
@@ -280,7 +280,8 @@ bool GattCharacteristic::setValue(int mantissa, int exponent, int formatType, in
   int len = offset + getTypeLen(formatType);
 
   if (mValue == NULL) mValue = new uint8_t[len];
-  if (len > mValueLength) return false;
+  if (len > sizeof(mValue)/sizeof(mValue[0]))
+    return false;
 
   switch (formatType) {
     case FORMAT_SFLOAT:
