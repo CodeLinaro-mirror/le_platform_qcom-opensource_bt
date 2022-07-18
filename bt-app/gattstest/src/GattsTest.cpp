228f058b5e4891788e4a38827fedd1471b1b8efd
gaoxiang <quic_gaoxiang@quicinc.com>
2022-07-18 16:18:37 +0800

    BT_APP : Fix the btapp print incorrect PHY info issue.
    Change-Id: Ibff2669dd804fe92872e0e27809f6ac4ff69db5e
    CRs-Fixed: 3244542

--- bt-app/gattstest/src/GattsTest.cpp
+++ bt-app/gattstest/src/GattsTest.cpp
@@ -368,7 +368,7 @@ void gattstestServerCallback::onPhyUpdate(string deviceAddress,int txPhy, int rx
 
 void gattstestServerCallback::onPhyRead(string deviceAddress,int txPhy,int rxPhy,int status)
 {
-  fprintf(stdout,"%s deviceAddress: %s, txPhy: %d rxPhy: %d status: %d", __FUNCTION__,
+  fprintf(stdout,"%s - deviceAddress: %s, txPhy: %d rxPhy: %d status: %d \n", __FUNCTION__,
                                                     deviceAddress.c_str(), txPhy, rxPhy, status);
   ALOGD(LOGTAG"%s deviceAddress: %s, txPhy: %d rxPhy: %d status: %d",__FUNCTION__,
                                                     deviceAddress.c_str(), txPhy, rxPhy, status);
