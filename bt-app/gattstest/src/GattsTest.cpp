5336d3a8fb3ff66815a21981c1013b14d59cf6f2
Chenhong Zhou <quic_chezhou@quicinc.com>
2023-07-12 11:22:52 +0530

    change gatt server transport from auto to LE
    Change-Id: I094a07745755915cfb640ea4269b3a03dfcfd014

--- bt-app/gattstest/src/GattsTest.cpp
+++ bt-app/gattstest/src/GattsTest.cpp
@@ -59,7 +59,8 @@ using namespace gatt;
 #define ADV_CFG_FILE_PATH "/data/misc/bluetooth/AdvertiserConfigFile.txt"
 #define GATT_SUCCESS 0
 #define AUTO_CONNECT 0
-#define TRANSPORT 0
+#define ATUO_TRANSPORT 0
+#define LE_TRANSPORT 2
 #define MAX_SERVER_INSTANCE 20
 #define MAX_SERVICE_INSTANCE 5
 #define INVALID_VALUE -1
@@ -171,10 +172,10 @@ void gattstestServerCallback::onConnectionStateChange(string deviceAddress, int 
       mServer= ptr->second;
       if(dtr != DeviceMap.end()) {
       //Device Already exists do not add
-    } else {
-      DeviceMap.insert(pair <string,GattServer*> (deviceAddress,mServer));
-    }
-    mServer->connect(deviceAddress,AUTO_CONNECT);
+      } else {
+        DeviceMap.insert(pair <string,GattServer*> (deviceAddress,mServer));
+      }
+      mServer->connect(deviceAddress,AUTO_CONNECT);
     }
   } else if(newState == GattDevice::STATE_DISCONNECTED) {
     fprintf(stdout,"The device %s got disconnected \n", deviceAddress.c_str());
@@ -650,7 +651,7 @@ void GattsTest::AddServer()
     }
     fprintf(stdout,"Adding Server %d \n", num_of_server);
     ALOGD(LOGTAG"Adding Server Instance : %d", num_of_server);
-    mgattServer = new GattServer(g_gatt,TRANSPORT);
+    mgattServer = new GattServer(g_gatt, LE_TRANSPORT);
     servInstanceMap.insert(pair <int,GattServer*> (num_of_server,mgattServer));
     ALOGD(LOGTAG"Adding Server CallBack : %d ", num_of_server);
     gattstestServerCb = new gattstestServerCallback();
