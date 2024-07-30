1668e32ba24da0d3c1066e76898e3df1ba2d5b53
Prathibha Madugonde <quic_prathm@quicinc.com>
2024-07-30 16:19:34 +0530

    btapp: Converting Manudata hex string to vector values
    Change-Id: Ia70ce4fcabb1125ff93148e0a45fec65a521a3ed

--- bt-app/gattctest/src/GattcTest.cpp
+++ bt-app/gattctest/src/GattcTest.cpp
@@ -1512,6 +1512,16 @@ bool GattcTest :: scanFilter(int filterType, string value)
   return true;
 }
 
+std::vector<uint8_t> hexStringToVector(const std::string& hex) {
+    std::vector<uint8_t> result;
+    for (size_t i = 0; i < hex.length(); i += 2) {
+        std::string byteString = hex.substr(i, 2);
+        uint8_t byte = static_cast<uint8_t>(std::stoi(byteString, nullptr, 16));
+        result.push_back(byte);
+    }
+    return result;
+}
+
 void GattcTest :: scanFilterManuData(int manuId, string manuData,
     string manuMask)
 {
@@ -1526,7 +1536,8 @@ void GattcTest :: scanFilterManuData(int manuId, string manuData,
     return;
   }
   fprintf(stdout, "Manu Data %s\n", manuData.c_str());
-  std::vector<uint8_t> vManuData(manuData.begin(), manuData.end());
+  //std::vector<uint8_t> vManuData(manuData.begin(), manuData.end());
+  std::vector<uint8_t> vManuData = hexStringToVector(manuData);
   for (auto i = vManuData.begin(); i != vManuData.end(); ++i) {
     cout<<(*i);
   }
